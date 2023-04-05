fc5476ff6f2c717b94a624be6a4d46a8c5a72f8a
viettdnde140214 <viettdnde140214@outlook.com>
2023-04-05 15:02:49 +0700

    set up database

---
+++ LeaveManagement_WebMVC/Data/LeaveAllocation.cs
@@ -0,0 +1,16 @@
+﻿using LeaveManagement_WebMVC.Data.Base;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagement_WebMVC.Data
+{
+    public class LeaveAllocation : BaseEntity
+    {
+        public int NumberOfDays { get; set; }
+
+        [ForeignKey("LeaveTypeId")]
+        public LeaveType LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+
+        public string EmployeeId { get; set; }
+    }
+}
---
+++ LeaveManagement_WebMVC/Data/LeaveType.cs
@@ -0,0 +1,10 @@
+﻿using LeaveManagement_WebMVC.Data.Base;
+
+namespace LeaveManagement_WebMVC.Data
+{
+    public class LeaveType : BaseEntity
+    {
+        public string Name { get; set; }
+        public int DefaultDays { get; set; }
+    }
+}
